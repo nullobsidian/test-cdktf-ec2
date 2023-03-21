19b1194025c9de253b67f51422ed39837cb2a9cf
moses <moses@node-ts-02.attlocal.net>
2023-03-21 03:07:08 +0000

    inital cdktf

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { Construct } from "constructs";
+import { App, TerraformStack, TerraformOutput, S3Backend } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { Instance } from "@cdktf/provider-aws/lib/instance";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new AwsProvider(this, "AWS", {
+      region: "us-east-1",
+    });
+
+    new S3Backend(this, {
+      bucket: "test-api-cdktf",
+      key: "terraform.tfstate",
+      region: "us-east-1",
+    });
+
+    const ec2Instance = new Instance(this, "compute", {
+      ami: "ami-0fd2c44049dd805b8",
+      instanceType: "t2.micro",
+    });
+
+    new TerraformOutput(this, "public_ip", {
+      value: ec2Instance.publicIp,
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, "test-cdktf-ec2");
+app.synth();
